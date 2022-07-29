fb30c61dff5dc9f147b94642f0cf14bc21fead04
LuisDFlores <floresluis242@gmail.com>
2022-07-28 23:54:41 -0400

    initializing

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+MongoClient.connect('mongodb-connection-string', (err, client) => {
+    // ... do something here
+})
+
+MongoClient.connect(connectionString, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+})
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3000, function () {
+    console.log('listening on 3000')
+})
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.body)
+})
+
